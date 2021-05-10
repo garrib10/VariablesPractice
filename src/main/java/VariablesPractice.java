86189c19cdf7ee2f9b520c575aa1ac34f43f43f9
Brian Garrity <briangarrity@Brians-MacBook-Air.local>
2021-05-10 19:03:39 -0400

    constructors,getters, and setters oh my

---
+++ src/main/java/VariablesPractice.java
@@ -0,0 +1,22 @@
+public class VariablesPractice {
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        double b = 6.5;
+        String c = "cat";
+
+        // calculated variable sum is a double
+        double sum = a + b;
+
+        // calculated variable concat is a String
+        String concat = a + c;
+
+        // value of sum is 7.5
+        System.out.println(sum);
+
+        // value of concat is "4cat"
+        System.out.println(concat);
+
+    }
+}
